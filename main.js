94d41527dd4c7a5c61601dc74ff189a6f8d6271c
Raven <purplebox02@gmail.com>
2022-04-22 16:38:21 +0300

    JS Updated

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
+//-------------------------------------------------------------
+// Adding Tasks To The List
+//-------------------------------------------------------------
 let task = document.getElementById('task-adding-form');
 let itemList = document.getElementById('itemList');
 
 task.addEventListener('submit', addItem);
 
+// COLOR & LABEL Defined Before The Initial Func.
 let itemBgc;
+let labelDisplay = 'Day';
 document.getElementById('order-select').addEventListener('change', function(e) {
-    if (e.target.value === "Today") {
+    if (e.target.value === "Secondary") {
         itemBgc = 'bg-primary text-white';
-    } else if (e.target.value === "Yesterday") {
+        labelDisplay = 'Secondary';
+    } else if (e.target.value === "Tertiary") {
         itemBgc = 'bg-success text-white';
-    } else if (e.target.value === "Tomorrow") {
+        labelDisplay = 'Tertiary';
+    } else if (e.target.value === "Primary") {
         itemBgc = 'bg-warning';
+        labelDisplay = 'Primary';
     } else {
         itemBgc = 'bg-light';
+        labelDisplay = 'Priority';
     }
 });
 
+function charCount(str, letter) {
+    let letterCount = 0;
+    for (let position = 0; position < str.length; position++) {
+        if (str.charAt(position) == letter) {
+            letterCount += 1;
+        }
+    }
+  return letterCount;
+}
+
 function addItem(e) {
     e.preventDefault();
+
     let newItem = document.querySelector('#item').value;
     let li = document.createElement('li');
-    li.className = `list-group-item d-flex ${itemBgc}`;
-
-
+    li.className = `list-group-item ${itemBgc}`;
 
     let textP = document.createElement('p');
     textP.className = 'px-2 flex-grow-1';
@@ -37,48 +55,103 @@ function addItem(e) {
     liContainer.ariaLabel = 'Button group with nested dropdown';
     li.appendChild(liContainer);
 
-    let checkBtn = document.createElement('button');
-    let xBtn = document.createElement('button');
-    checkBtn.className = 'btn btn-info text-white';
-    xBtn.className = 'btn btn-info text-white';
-    checkBtn.type = 'button';
-    xBtn.type = 'button';
     const checkMark = document.createElement('i');
-    checkMark.className = 'fa-solid fa-check';
+    checkMark.className = 'fa-solid fa-check text-white bg-info done-mark list-check-btn';
     const xMark = document.createElement('i');
-    xMark.className = 'fa-solid fa-xmark';
-    checkBtn.appendChild(checkMark);
-    xBtn.appendChild(xMark);
-    liContainer.appendChild(checkBtn);
-    liContainer.appendChild(xBtn);
+    xMark.className = 'fa-solid fa-xmark text-white bg-info delete list-x-btn';
+    liContainer.appendChild(checkMark);
+    liContainer.appendChild(xMark);
     li.appendChild(liContainer);
 
-    let menuContainer = document.createElement('div');
-    menuContainer.className = 'day-selector-menu';
-    let formSelect = document.createElement('select');
-    formSelect.className = 'form-select bg-info text-white border-info';
-    formSelect.ariaLabel = 'Default select example';
-    menuContainer.appendChild(formSelect);
-    let optionDay = document.createElement('option');
-    optionDay.defaultSelected = true;
-    optionDay.textContent = 'Day';
-    formSelect.appendChild(optionDay);
-    let optionYesterday = document.createElement('option');
-    optionYesterday.className = 'bg-success';
-    optionYesterday.setAttribute('value', 'Yesterday');
-    optionYesterday.textContent = 'Yesterday';
-    formSelect.appendChild(optionYesterday);
-    let optionToday = document.createElement('option');
-    optionToday.className = 'bg-primary';
-    optionToday.setAttribute('value', 'Today');
-    optionToday.textContent = 'Today';
-    formSelect.appendChild(optionToday);
-    let optionTomorrow = document.createElement('option');
-    optionTomorrow.className = 'bg-warning';
-    optionTomorrow.setAttribute('value', 'Tomorrow');
-    optionTomorrow.textContent = 'Tomorrow';
-    formSelect.appendChild(optionTomorrow);
-    li.appendChild(menuContainer);
+    let displayContainer = document.createElement('div');
+    displayContainer.className = 'displayer-container bg-info rounded';
+    let labelHolder = document.createElement('h6');
+    labelHolder.className = 'text-white';
+    labelHolder.textContent = `${labelDisplay}`;
+    displayContainer.appendChild(labelHolder);
+    li.appendChild(displayContainer);
     
-    itemList.appendChild(li);
+    if (newItem.length && charCount(newItem, " ") !== newItem.length) {
+        itemList.appendChild(li);
+        document.getElementById('item').value ='';
+    } else {
+        alert('Tasks Cannot Be Empty');
+    }
+}
+
+//-------------------------------------------------------------
+// X Button to Remove Items & Check If User is Sure
+//-------------------------------------------------------------
+itemList.addEventListener('click', removeItem);
+doneList.addEventListener('click', removeItem);
+
+let toastPopup = document.getElementById('popup-toast');
+
+function removeItem(e) {
+    if(e.target.classList.contains('delete')){
+        let li = e.target.parentElement.parentElement;
+        let toast = new bootstrap.Toast(toastPopup);
+        toast.show();
+        toastPopup.addEventListener('click', youSure);
+        // let input = document.getElementsByTagName('i');
+        // input.addEventListener("keyup", function(event) {
+        //     if (event.keyCode === 13) {
+        //         event.preventDefault();
+        //         document.getElementByClassName('check-sure').click();
+        //     }
+        //   });
+        
+        function youSure(f) {
+            if (f.target.classList.contains('check-sure')) {
+                if (itemList.contains(li)) {
+                    itemList.removeChild(li);
+                } else if (doneList.contains(li)) {
+                    doneList.removeChild(li);
+                }
+            }
+        }
+    }
+}
+
+//-------------------------------------------------------------
+// Check Button to Move Items to Done-List
+//-------------------------------------------------------------
+itemList.addEventListener('click', doneItem);
+
+function doneItem(d) {
+    if (d.target.classList.contains('done-mark')) {
+        let li = d.target.parentElement.parentElement;
+        li.className = 'list-group-item d-flex bg-dark bg-gradient text-white';
+        li.removeChild(li.lastElementChild);
+        li.removeChild(li.lastElementChild);
+
+        let btnContainer = document.createElement('div');
+        li.appendChild(btnContainer);
+
+        const xMark = document.createElement('i');
+        xMark.className = 'fa-solid fa-xmark delete';
+        btnContainer.appendChild(xMark);
+
+        doneList.appendChild(li);
+    }
+}
+
+//-------------------------------------------------------------
+// Search Filter
+//-------------------------------------------------------------
+let filter = document.getElementById('filter');
+filter.addEventListener('keyup', filterItems);
+
+function filterItems(e) {
+    let text = e.target.value.toLowerCase();
+    let items = itemList.getElementsByTagName('li');
+
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    })
 }
